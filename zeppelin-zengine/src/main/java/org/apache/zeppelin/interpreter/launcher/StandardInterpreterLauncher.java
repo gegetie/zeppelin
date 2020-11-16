1987e14e374d1a202fd52e7d32ef27b45a28b4e5
jeffzhang.zjf <jeffzhang.zjf@alibaba-inc.com>
2020-11-16 22:20:16 +0800

    support security cluster

--- zeppelin-zengine/src/main/java/org/apache/zeppelin/interpreter/launcher/StandardInterpreterLauncher.java
+++ zeppelin-zengine/src/main/java/org/apache/zeppelin/interpreter/launcher/StandardInterpreterLauncher.java
@@ -18,7 +18,6 @@
 
 package org.apache.zeppelin.interpreter.launcher;
 
-import org.apache.commons.exec.environment.EnvironmentUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.zeppelin.conf.ZeppelinConfiguration;
 import org.apache.zeppelin.interpreter.InterpreterOption;
@@ -55,6 +54,7 @@ public class StandardInterpreterLauncher extends InterpreterLauncher {
     String name = context.getInterpreterSettingName();
     int connectTimeout = getConnectTimeout();
     int connectionPoolSize = getConnectPoolSize();
+    enhanceKeytabProperties(context);
 
     if (option.isExistingProcess()) {
       return new RemoteInterpreterRunningProcess(
@@ -77,6 +77,67 @@ public class StandardInterpreterLauncher extends InterpreterLauncher {
           zConf.getInterpreterDir() + "/" + groupName, localRepoPath,
           buildEnvFromProperties(context), connectTimeout, connectionPoolSize, name,
           context.getInterpreterGroupId(), option.isUserImpersonate(), context.getClusterId());
+    }
+  }
+
+
+  private void enhanceKeytabProperties(InterpreterLaunchContext context) {
+    String[] tokens = context.getClusterId().split("-");
+    if (tokens.length != 2) {
+      LOGGER.info("It is not a valid remote clusterId: " + context.getClusterId()
+              + ", Skip enhanceKeytabProperties");
+      return;
+    }
+    if (context.getInterpreterSettingName().equalsIgnoreCase("hive")) {
+      boolean hiveKeytabExist = checkKeytab(context, "/etc/ecm/hive-conf/hive.keytab");
+      if (hiveKeytabExist) {
+        if (tokens.length != 2) {
+          throw new RuntimeException("Invalid clusterId: " + context.getClusterId());
+        }
+        LOGGER.info("Enhance hive interpreter properties with keytab info");
+        String principal = "hive/emr-header-1.cluster-" + tokens[1] + "@EMR." + tokens[1] + ".COM";
+        context.getProperties().put("zeppelin.jdbc.keytab.location", "/etc/ecm/hive-conf/hive.keytab");
+        context.getProperties().put("zeppelin.jdbc.principal", principal);
+        context.getProperties().put("default.principal", principal);
+        context.getProperties().put("zeppelin.jdbc.auth.type", "kerberos");
+      }
+    } else if (context.getInterpreterSettingName().equalsIgnoreCase("spark")) {
+      boolean sparkKeytabExist = checkKeytab(context, "/etc/ecm/spark-conf/spark.keytab");
+      if (sparkKeytabExist) {
+        if (tokens.length != 2) {
+          throw new RuntimeException("Invalid clusterId: " + context.getClusterId());
+        }
+        LOGGER.info("Enhance spark interpreter properties with keytab info");
+        String principal = "spark/emr-header-1.cluster-" + tokens[1] + "@EMR." + tokens[1] + ".COM";
+        context.getProperties().put("spark.yarn.keytab", "/etc/ecm/spark-conf/spark.keytab");
+        context.getProperties().put("spark.yarn.principal", principal);
+      }
+    }
+  }
+
+  /**
+   * Add keytab info if necessary.
+   *
+   * @param context
+   * @return
+   */
+  private boolean checkKeytab(InterpreterLaunchContext context, String keytab) {
+    try {
+      Process process = Runtime.getRuntime().exec(new String[]{"ssh", getRemoteHost(context.getClusterId()),
+              "ls " + keytab});
+      int exitCode = process.waitFor();
+      return exitCode == 0;
+    } catch (Exception e) {
+      LOGGER.error("Fail to check keytab: " + keytab, e.getMessage());
+    }
+    return false;
+  }
+
+  private String getRemoteHost(String clusterId) {
+    if (clusterId.equalsIgnoreCase("localhost")) {
+      return "localhost";
+    } else {
+      return "emr-header-1." + clusterId;
     }
   }
 
